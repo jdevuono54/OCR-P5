c8fc848ec6e650518c057f64c027f95c98a3587b
Jdevuono <jacques.devuono@gmail.com>
2021-11-23 19:38:52 +0100

    linter

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,41 +1,43 @@
 $(document).ready(() => {
-    function onKonamiCode(cb) {
-        let input = '';
-        const key = '38384040373937396665';
+  function onKonamiCode(cb) {
+    let input = '';
+    const key = '38384040373937396665';
 
-        document.addEventListener('keydown', e => {
-            input += ("" + e.keyCode);
+    document.addEventListener('keydown', (e) => {
+      input += (`${e.keyCode}`);
 
-            if (input === key) {
-                return cb();
-            }
+      if (input === key) {
+        return cb();
+      }
 
-            if (!key.indexOf(input)) return;
+      if (!key.indexOf(input)) return 1;
 
-            input = ("" + e.keyCode);
-        });
-    }
+      input = (`${e.keyCode}`);
 
-    onKonamiCode(function () {
-        const img = new Image();
-
-        img.src = `../images/konami/k${Math.floor(Math.random() * 5)}.webp`;
-        img.style.width = '350px';
-        img.style.height = '300px';
-        img.style.transition = '6s all linear';
-        img.style.position = 'fixed';
-        img.style.left = '-400px';
-        img.style.bottom = '-5px';
-        img.style.zIndex = 999999;
-
-        document.body.appendChild(img)
-
-        window.setTimeout(() => {
-            img.style.left = 'calc(100% + 500px)'
-        }, 50)
-
-        window.setTimeout(() => {
-            img.parentNode.removeChild(img)
-        }, 6000)
+      return 1;
     });
+  }
+
+  onKonamiCode(() => {
+    const img = new Image();
+
+    img.src = `../images/konami/k${Math.floor(Math.random() * 5)}.webp`;
+    img.style.width = '350px';
+    img.style.height = '300px';
+    img.style.transition = '6s all linear';
+    img.style.position = 'fixed';
+    img.style.left = '-400px';
+    img.style.bottom = '-5px';
+    img.style.zIndex = 999999;
+
+    document.body.appendChild(img);
+
+    window.setTimeout(() => {
+      img.style.left = 'calc(100% + 500px)';
+    }, 50);
+
+    window.setTimeout(() => {
+      img.parentNode.removeChild(img);
+    }, 6000);
+  });
 });
